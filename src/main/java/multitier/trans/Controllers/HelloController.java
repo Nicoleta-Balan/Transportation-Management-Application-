8ce1952df941eed24874ae00b75954d27f4332cc
NicoletaChihaia <c_nicoleta00@yahoo.it>
2025-10-22 13:18:03 +0300

    trying the config with private repository

--- src/main/java/multitier/trans/Controllers/HelloController.java
+++ src/main/java/multitier/trans/Controllers/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Test";
+        return "Test private repo";
     }
 
 }
